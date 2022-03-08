274c69e65d377f7c18bf53ef7f2e6652f97421cc
Michael Sergio <mikeserg@gmail.com>
2022-03-07 22:40:27 -0500

    Use 16 bit X instead of mucking with A register size

--- snes/joycon.asm
+++ snes/joycon.asm
@@ -3,17 +3,9 @@
     and #$01    ; Check low bit to see if ready to be read.
     bne end_joycon_read
 
-    .a16
-    rep #$30    ; A/X/Y - 16 bit
-
     ; read joycon data (registers 4218h ~ 421Fh)
-    lda JOY1L   ; Controller 1 as 16 bit.
-    sta wJoyVar
-    lda #$0     ; Clear out the high bits of the A register
-                ; otherwise this messes with others ops
-
-    sep #$20    ; Go back to A 8-bit
-    .a8
+    ldx JOY1L   ; Controller 1 as 16 bit.
+    stx wJoyVar
 
     end_joycon_read:
 .endmacro
