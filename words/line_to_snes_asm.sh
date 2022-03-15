23db47b98b7fefd5977034f6b30a061883a00a7d
Michael Sergio <mikeserg@gmail.com>
2022-03-15 01:50:46 -0400

    Use upper case words

--- words/line_to_snes_asm.sh
+++ words/line_to_snes_asm.sh
@@ -1,3 +1,3 @@
 # Give a file name
-#awk '{printf ".asciiz \"%s\"\n", $1}' common5_shuf_478.txt
-awk '{printf ".asciiz \"%s\"\n", $1}' $1
+#awk '{printf ".asciiz \"%s\"\n", toupper($1)}' common5_shuf_478.txt
+awk '{printf ".asciiz \"%s\"\n", toupper($1)}' $1
