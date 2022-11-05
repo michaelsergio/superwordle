b502828ab8dd1b0015d1eee3d478f77b23e9d1d5
Mike Sergio <mikeserg@gmail.com>
2022-11-05 02:49:37 -0400

    Fixed controls

--- snes/joycon.asm
+++ snes/joycon.asm
@@ -1,11 +1,15 @@
 .macro joycon_read wJoyVar
+    joycon_read_in_progress:
     lda HVBJOY   ; auto-read joypad status
     and #$01    ; Check low bit to see if ready to be read.
-    bne end_joycon_read
+    
+    ; wait for 0 to ready to read data
+    bne joycon_read_in_progress
+
+    joycon_ready_to_read:
 
     ; read joycon data (registers 4218h ~ 421Fh)
-    ldx JOY1L   ; Controller 1 as 16 bit.
+    ldx JOY1L    ; Controller 1 as 16 bit.
     stx wJoyVar
-
     end_joycon_read:
 .endmacro
